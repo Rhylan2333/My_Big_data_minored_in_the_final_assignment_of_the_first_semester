7335e284c67495b2e6bf3f5939cb58c126b772e0
Yuhao Cai <caucaicai@qq.com>
2022-01-29 13:20:45 +0800

    我混用代码了，学会了在传入 render_template() 函数的关键字参数中，左边的 movies 是模板中使用的变量名称，右边的 movies 则是该变量指向的实际对象。这里传入模板的 name 是字符串，movies 是列表，但能够在模板里使用的不只这两种 Python 数据结构，你也可以传入元组、字典、函数等。

--- app.py
+++ app.py
@@ -5,14 +5,26 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == "POST":
-        X1 = eval(request.form.get('left')) / 50
-        X2 = eval(request.form.get('right')) / 300
-        Y0 = formula.cal_the_complex_of_1_and_2_generation_of_Ha_0(X1, X2)
-        return render_template('index.html', RESULT=str(Y0))
-    return render_template('index.html')
-
+# def index():
+#     if request.method == "POST":
+#         X1 = eval(request.form.get('left')) / 50
+#         X2 = eval(request.form.get('right')) / 300
+#         Y0 = formula.cal_the_complex_of_1_and_2_generation_of_Ha_0(X1, X2)
+#         return render_template('index.html', RESULT=str(Y0))
+#     return render_template('index.html')
+# 返回渲染好的模板作为响应
+def show_list_ha():
+# 定义虚拟数据
+    name_user = '蔡雨豪'
+    list_ha = [
+        {'x1': '50', 'x2': '300', 'y': '25.624243', 'date': '2022-01-29', },
+        {'x1': '10', 'x2': '60', 'y': '15.435247', 'date': '2022-01-25', },
+        {'x1': '20', 'x2': '120', 'y': '17.6739028', 'date': '2022-01-26', },
+        {'x1': '30', 'x2': '180', 'y': '20.1182874', 'date': '2022-01-27', },
+        {'x1': '40', 'x2': '250', 'y': '22.768400800000002', 'date': '2022-01-28', },
+    ]
+    return render_template('index.html', name_user=name_user, list_ha=list_ha)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
