eced0a6ee84d33d60d3a8a15a05bab5c920b1f45
Yuhao Cai <caucaicai@qq.com>
2022-02-02 06:45:57 +0800

    吃了不会用 Vim 的亏，只好从 Pythonanywhere 里的 Bash 中获取密码，在自己电脑写入后再 push，然后那端再 clone。

--- wsgi.py
+++ wsgi.py
@@ -5,6 +5,7 @@ import os
 from dotenv import load_dotenv
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+print(dotenv_path)
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
