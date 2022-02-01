764f17af498ecbed4ed6ea2fc91833f842c448d8
Yuhao Cai <caucaicai@qq.com>
2022-02-01 21:21:28 +0800

    模糊查询

--- test_query.py
+++ test_query.py
@@ -5,3 +5,8 @@ print("id_user 是 "+str(id_user))
 
 id_area=Area_info.query.filter_by(name_area='伊犁').first().id_area  # 就在这外键连接，用 filter_by()
 print("id_area 是 "+str(id_area))
+
+list_name_area=Area_info.query.filter(Area_info.name_area.like("%{}%".format('伊'))).all()  # 返回一个 list
+print("\n模糊查询结果： ")
+for name_area in list_name_area:
+    print("\t" + str(name_area.name_area))
